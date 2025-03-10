4a0faef0775c9f9d5ce08526bff9350f3b26ba13
hunaid-nawaz <hunaid.nawaz@gmail.com>
2025-03-10 14:36:58 +0500

    Add tailwind.css in the react app.

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: "class", // This enables dark mode
+  content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
