779ca6a55cac9a839b987f0dac9b538a09c10d39
hunaid-nawaz <hunaid.nawaz@gmail.com>
2025-03-10 17:36:44 +0500

    Add Hero Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import Hero from './components/Hero'
 
 const App = () => {
   return (
     <div className='bg-[#0e0c1e]'>
       <Navbar/>
+      <Hero/>
     </div>
   )
 }
